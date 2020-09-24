a96be4f00b3ab95d6bb95295265a62df27ea21da
ROHITDH <44223447+ROHITDH@users.noreply.github.com>
2020-09-25 00:34:16 +0530

    Update main.py

--- SONNET GENERATOR OUTPUT/main.py
+++ SONNET GENERATOR OUTPUT/main.py
@@ -3,14 +3,15 @@
 #ada_lovelase_day_celebration
 #19/09/2020
 
-
 #import req. lib/package
 from keras.models import load_model
 import os
 import numpy as np
 
 
-#training text and vectorize to 3D tensor
+
+
+
 def load_data(max_length_seq=25, step=3):
     with open(os.path.join(os.getcwd(),os.path.join('res','dataset.txt')), 'r', encoding = 'utf8') as f:
         data = f.read().lower()
@@ -36,7 +37,40 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 #model loading and construction through prediction
-def generate_sonnet():
+#for sonnet lines
+def generate_sonnet(new_sonnet):
+    new_sonnet=new_sonnet.lower()[:40]
+    max_length_seq = len(new_sonnet)
+    step = 3
+   # new_gen = ""
+    data, unique_indices, uniques = load_data(max_length_seq, step)
+
+    model = load_model('sonnet_gen_model.h5')
+   # start_idx = np.random.randint(0, len(data) - max_length_seq - 1)
+   # new_sonnet = data[start_idx:start_idx + max_length_seq]
+    new_gen = new_sonnet
+    for i in range(625):
+
+        sampled = np.zeros((1, max_length_seq, len(uniques)))
+        for j, unique in enumerate(new_sonnet):
+            sampled[0, j, unique_indices[unique]] = 1.
+
+
+        preds = model.predict(sampled, verbose=0)[0]
+        pred_idx = sample(preds, temperature=0.5)
+        next_unique = uniques[pred_idx]
+
+        # Append unique and ready for next unique
+        new_sonnet += next_unique
+        new_sonnet = new_sonnet[1:]
+
+        new_gen += next_unique
+    return new_gen
+
+
+#model loading and construction through prediction
+#generate sonnet full
+def generate_sonnet_f():
     max_length_seq = 40
     step = 3
     new_gen = ""
@@ -66,15 +100,53 @@ def generate_sonnet():
     print("\n\n")
     return new_gen
 
-if __name__ == '__main__':
-    d=generate_sonnet().replace("\n\n","\n").split("\n")[1:15]
+
+
+def generate_sonnet_lines(inp):
+    if len(inp)<40:
+        inp=inp+(40-len(inp))*" "
+
+    d=generate_sonnet(inp).replace("\n\n","\n").split("\n")[1:14]
+    print(inp)
+    for i in(d):
+        print(i.strip())
+
+        
+def generate_sonnet_full():
+    d=generate_sonnet_f().replace("\n\n","\n").split("\n")[1:15]
     d1=d.pop(0).split(" ")
 
-    if(d1[0].lower()=='and' or d1[0].lower()=='but'):d1[0]="As"
+    if(d1[0]=="'" or d1[0]=='"'or d1[0]==':'or d1[0]==';'or d1[0]==','or d1[0]=='.'):
+        d1[0]=""
+        if(d1[1].lower()=='and' or d1[1].lower()=='but' or d1[1].lower()=='And' or d1[1].lower()=='But'):
+            d1[1]="As"
+    elif(d1[0].lower()=='and' or d1[0].lower()=='but' or d1[0].lower()=='And' or d1[0].lower()=='But'):
+        d1[0]="As"
 
-    for i in([" ".join(d1).capitalize()]+d):
-        print(i)
+    d2 = d.pop().split(" ")
+    if(d2[-1].lower()=='and' or d2[-1].lower()=='but' or d2[-1].lower()=='And' or d2[-1].lower()=='But'):
+        d2[-1]="."
 
+    d = [" ".join(d1).strip().capitalize()]+d+[" ".join(d2).strip()]
+    k=0
+    for i in d:
+        d[k]=i.strip()
+        k+=1
+    for line in d:
+        print(line)
+   
+    
+inp = input("ENTER >> ")
+if len(inp) <=25:
+    print("Your input sequence is too short, \n So generating whole sonnet:")
+    generate_sonnet_full()
 
+elif len(inp) >= 90:
+    print("Your input sequence is too large, \n So generating whole sonnet:")
+    generate_sonnet_full()
+else:
+    generate_sonnet_lines(inp)
+    
+    
 ##########  attributions  ##############
 ## loosely based on theory on machinelearningmastery.com by Jason Brownlee
